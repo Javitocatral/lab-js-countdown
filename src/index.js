87577170280fe76013883502cfe3846a1a7fbcd6
Javier Gascon <javitocatral@gmail.com>
2024-09-08 19:37:35 +0200

    lab terminado

--- src/index.js
+++ src/index.js
@@ -1,38 +1,58 @@
-const DURATION = 10; // 10 seconds
-let remainingTime = DURATION; // Countdown starting from 10
-let timer = null; // Variable to store the interval
-
-
+const DURATION = 10 // 10 seconds
+let remainingTime = DURATION // Countdown starting from 10
+let timer = null // Variable to store the interval
 
 // ITERATION 1: Add event listener to the start button
 
 // Your code goes here ...
 
-
-
-
+const botonStart = document.querySelector('#start-btn')
+const numeroContador = document.querySelector('#time')
+const toastNodo = document.querySelector('#toast')
+const mensajeNodo = document.querySelector('#toast-message')
+const botonDeCierre = document.querySelector('#close-toast')
 // ITERATION 2: Start Countdown
-function startCountdown() {
-  console.log("startCountdown called!");
 
-
+botonStart.addEventListener('click', () => {
+  empezarcCountador()
+  botonStart.disabled = true
+  showToast()
+})
+function empezarcCountador() {
+  console.log('startCountdown called!')
   // Your code goes here ...
+  timer = setInterval(() => {
+    remainingTime--
+    if (remainingTime >= 0) {
+      numeroContador.textContent = remainingTime
+    }
+
+    if (remainingTime === 10) {
+      showToast('⏰ Final countdown! ⏰')
+    } else if (remainingTime === 5) {
+      showToast('Start the engines! 💥')
+    } else if (remainingTime === 0) {
+      showToast('Lift off! 🚀')
+      clearInterval(timer)
+    }
+  }, 1000)
 }
-
-
-
 
 // ITERATION 3: Show Toast
 function showToast(message) {
-  console.log("showToast called!");
+  if (message) {
+    mensajeNodo.textContent = message
+  }
+  toastNodo.classList.add('show')
+  setTimeout(() => {
+    toastNodo.classList.remove('show')
+  }, 3000)
 
   // Your code goes here ...
-
-
-
-
+  botonDeCierre.addEventListener('click', () => {
+    toastNodo.classList.remove('show')
+  })
   // BONUS: ITERATION 4: TOAST CLOSE BUTTON
 
   // Your code goes here ...
-
 }
